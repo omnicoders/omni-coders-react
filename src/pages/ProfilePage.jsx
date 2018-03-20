ed9e8cc299e80d33b42a7a5642c8478f15cb6117
matthewstewart <matthew.stewart.mi@gmail.com>
2018-03-20 16:11:54 -0700

    edit profile

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -26,14 +26,6 @@ class ProfilePage extends Component {
                     </p>
                   </div>
                   <ul className="list-group list-group-flush">
-                    <Link 
-                      className="list-group-item list-group-item-action bg-success text-light"
-                      to="/login"
-                    >Login</Link>
-                    <Link 
-                      className="list-group-item list-group-item-action bg-primary text-light"
-                      to="/signup"
-                    >Sign Up</Link>
                     { this.props.isLoggedIn ? (
                       <button
                         className="list-group-item list-group-item-action bg-secondary text-light"
